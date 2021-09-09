a1cfc172420d463b3de70800b178ec6f4261dbea
João Paulo Dias França <j.pdf00@gmail.com>
2021-09-09 16:58:50 -0300

    Add json_web_token wrapper

---
+++ app/models/json_web_token.rb
@@ -0,0 +1,9 @@
+class JsonWebToken
+  def self.encode(payload)
+    JWT.encode payload, Rails.application.secrets.secret_key_base, 'HS256'
+  end
+
+  def self.decode(token)
+    JWT.decode(token, Rails.application.secrets.secret_key_base, 'HS256').first
+  end
+end
