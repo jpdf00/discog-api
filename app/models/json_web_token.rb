339c997a8f767932af6708d53f2d042b74716b03
João Paulo Dias França <j.pdf00@gmail.com>
2021-09-10 07:08:32 -0300

    Turns json-web-token into module

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,4 +1,4 @@
-class JsonWebToken
+module JsonWebToken
   def self.encode(payload)
     expiration = 7.days.from_now.to_i
     JWT.encode payload.merge(exp: expiration), Rails.application.secrets.secret_key_base, 'HS256'
