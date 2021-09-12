e1f9968c14f55c19819ceab0d44a705e06c241f5
João Paulo Dias França <j.pdf00@gmail.com>
2021-09-11 23:39:57 -0300

    Add size_per_song on album_serializer

--- app/serializers/album_serializer.rb
+++ app/serializers/album_serializer.rb
@@ -1,6 +1,10 @@
 class AlbumSerializer < ActiveModel::Serializer
-  attributes :id, :name, :year, :qtd_songs, :size, :cover
+  attributes :id, :name, :year, :qtd_songs, :size, :size_per_song, :cover
 
   belongs_to :band
   belongs_to :type
+
+  def size_per_song
+    (@object.size / @object.qtd_songs).floor(2)
+  end
 end
