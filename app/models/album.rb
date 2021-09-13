6f5f86b0d3b50483dd5397d8506146abe930bf49
João Paulo Dias França <j.pdf00@gmail.com>
2021-09-13 10:58:26 -0300

    Add validations for album model

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,4 +3,9 @@ class Album < ApplicationRecord
   belongs_to :type
 
   mount_uploader :cover, CoverUploader
+
+  validates :name, presence: true, uniqueness: true
+  validates :year, numericality: true
+  validates :qtd_songs, numericality: true
+  validates :size, numericality: true
 end
