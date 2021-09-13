88cfdc112493e789da83c12ba80a7c06b2e928a4
João Paulo Dias França <j.pdf00@gmail.com>
2021-09-13 10:11:34 -0300

    Add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   has_many :bands
 
   mount_uploader :avatar, AvatarUploader
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: :not_an_email }, presence: true, uniqueness: true
+  validates :username, presence: true
+
+  scope :without_admins, -> { where(admin: false)}
+
+  before_save :default_values
+
+  private
+    def default_values
+      self.admin = false if self.admin.nil?
+    end
 end
